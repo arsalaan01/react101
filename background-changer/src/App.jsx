f57d5fc982b9c03f06fb82a31502f991583fe71b
arsalaan <arsalaan0101@gmail.com>
2025-06-30 20:36:46 +0530

    password generator

--- background-changer/src/App.jsx
+++ background-changer/src/App.jsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
 import './App.css';
+import BGChanger from './components/BGChanger';
+import PasswordGenerator from './components/PasswordGenerator';
 
 const App = () => {
 
-  const [color,setColor] = useState('crimson');
-
-
-
   return (
     <>
-      <div className="w-full h-screen duration-200" style={{backgroundColor:color}}></div>
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button className="outline-none rounded-full px-4 py-1 text-white shadow-lg" style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>Red</button>
-          <button className="outline-none rounded-full px-4 py-1 text-white shadow-lg" style={{backgroundColor:"olive"}} onClick={()=>setColor("olive")}>Olive</button>
-          <button className="outline-none rounded-full px-4 py-1 text-white shadow-lg" style={{backgroundColor:"purple"}} onClick={()=>setColor("purple")}>Purple</button>
-          <button className="outline-none rounded-full px-4 py-1 text-white shadow-lg" style={{backgroundColor:"green"}} onClick={()=>setColor("green")}>Green</button>
-          <button className="outline-none rounded-full px-4 py-1 text-white shadow-lg" style={{backgroundColor:"white",color:"black"}} onClick={()=>setColor("white")}>Reset</button>
-        </div>
-      </div>
+      {/* <BGChanger /> */}
+      <PasswordGenerator />
     </>
   )
 }
