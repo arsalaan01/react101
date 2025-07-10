cea1f7bf1b8b46fdff2793d1820d15ed1357ecba
arsalaan01 <arsalaan0101@gmail.com>
2025-07-10 22:27:01 +0530

    megablog intial setup

---
+++ megablog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  return (
+    <>
+      <h1>Welcome to Mega Blog Application</h1>
+    </>
+  )
+}
+
+export default App
---
+++ megablog/src/appwrite/config.js
@@ -0,0 +1,4 @@
+import conf from "../conf/conf";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+
