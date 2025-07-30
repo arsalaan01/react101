8c9e60b1eecf06a487d594278dabf007ab8326c5
arsalaan01 <arsalaan0101@gmail.com>
2025-07-30 22:15:37 +0530

    mochi.design clone

---
+++ ochi-design/src/components/Landing.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaArrowUpLong } from "react-icons/fa6";
+
+const Landing = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="textstructure mt-50 px-20">
+        {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
+          return (
+            <div className="masker">
+              <div className="w-fit flex items-center overflow-hidden">
+                { index == 1 && (<div className="mr-5 w-[8vw] rounded-md h-[5.5vw] relative bg-[#b7d154]"></div>)}
+                <h1 className='uppercase text-[7vw] leading-[6.5vw] tracking-tight font-["Founders Grotesk"] font-medium'>
+                  {item}
+                </h1>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="border-t-[1px] border-zinc-800 mt-20 flex justify-between items-center py-5 px-20">
+        {[
+          "For public and private companies",
+          "From the first pitch to IPO",
+        ].map((item, index) => (
+          <p className="text-md font-light tracking-tight leading-none">
+            {item}
+          </p>
+        ))}
+        <div className="start flex gap-5 items-center">
+          <div className="px-4 py-2 border-[1px] border-zinc-400 font-light text-md uppercase rounded-full">
+            start the project
+          </div>
+          <div className="w-10 h-10 flex items-center justify-center rounded-full border-[1px] border-zinc-400">
+            <span className="rotate-[45deg]">
+              <FaArrowUpLong />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
